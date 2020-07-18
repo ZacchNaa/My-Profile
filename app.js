d6c77f14b972b476b6e3b7a02660b7d272a60645
Zacchaeus Napuo <2016.naaz@gmail.com>
2020-07-18 20:48:11 +0530

    Added process env

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.get("/", (req, res) => {
 });
 
 //setting up server
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
