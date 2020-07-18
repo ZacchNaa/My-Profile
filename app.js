d38652448b6003abe252905b32b77a21d9149070
Zacchaeus Napuo <2016.naaz@gmail.com>
2020-07-18 19:44:08 +0530

    Refactor code with major additions

---
+++ app.js
@@ -0,0 +1,22 @@
+//Packages
+const express = require("express");
+const bodyParser = require("body-parser");
+const _ = require("lodash");
+
+const app = express();
+
+//Template engine
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//ROUTING
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+//setting up server
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
